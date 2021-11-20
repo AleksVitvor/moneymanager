b5f9535c7a45faf6ebe8e6b74fabd79f5cbea3d4
Alexey_Vitvor <vitvor01@gmail.com>
2021-11-20 16:35:04 +0300

    databaseDev: Add azure functions

---
+++ ExchangeRateLibrary/Service/ExchangeRateService.cs
@@ -0,0 +1,21 @@
+﻿namespace ExchangeRateLibrary.Service
+{
+    using Model;
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Net.Http.Json;
+    using System.Threading.Tasks;
+
+    public class ExchangeRateService : IExchangeRateService
+    {
+        public async Task<ExchangeRateGeneralInfo> GetExchangeRate(DateTime date)
+        {
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://api.exchangeratesapi.io/v1/");
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return await httpClient.GetFromJsonAsync<ExchangeRateGeneralInfo>($"/{date.ToShortDateString()}?access_key=c217147a760efc64bbb43df91ffde2c1");
+        }
+    }
+}
