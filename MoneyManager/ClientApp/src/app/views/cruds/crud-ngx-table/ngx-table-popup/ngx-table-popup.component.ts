92f6def80a3b0f194562e465beea199e9cdecfb9
Alexey Vitvor <a.vitvor@syberry.com>
2022-06-15 19:44:02 +0300

    main: Fix period search

--- MoneyManager/ClientApp/src/app/views/cruds/crud-ngx-table/ngx-table-popup/ngx-table-popup.component.ts
+++ MoneyManager/ClientApp/src/app/views/cruds/crud-ngx-table/ngx-table-popup/ngx-table-popup.component.ts
@@ -107,7 +107,7 @@ export class NgxTablePopupComponent implements OnInit, OnDestroy {
       transactionDate: [item.transactionDate || '', Validators.required],
       isRepeatable: [item.isRepeatable || false],
       currencyId: ['', Validators.required],
-      transactionPeriodId: [item.transactionPeriod !== undefined ? this.transactionPeriods.find(x => x.viewValue === item.transactionPeriod).value : '']
+      transactionPeriodId: [item.transactionPeriod !== undefined && item.transactionPeriod !== null ? this.transactionPeriods.find(x => x.viewValue === item.transactionPeriod).value : '']
     }, { validator: repeatableRequired });
   }
 
