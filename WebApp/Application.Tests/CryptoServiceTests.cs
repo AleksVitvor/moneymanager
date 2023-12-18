92455e30d0a4b5811f5284912622d0f3b591bddc
Alexey Vitvor <avitvor@gmail.com>
2023-12-18 22:57:11 +0300

    Add cryptoservice tests

---
+++ WebApp/Application.Tests/CryptoServiceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Tests
+{
+    using NUnit.Framework;
+
+    using Services;
+
+    public class CryptoServiceTests
+    {
+        [Test]
+        public void EmptyString_Test()
+        {
+            var emptyString = string.Empty;
+            var expectedHash = "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855";
+
+            var emptyStringHash = CryptoService.ComputeHash(emptyString);
+            Assert.AreEqual(emptyStringHash, expectedHash);
+        }
+    }
+}
